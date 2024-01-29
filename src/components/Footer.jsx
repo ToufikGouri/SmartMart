4570000cfa4ea955c148a5417992a443b8de14ea
ToufikGouri <toufikgouri3@gmail.com>
2024-01-29 19:05:04 +0530

    100% Completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                     <li className='list-unstyled'> 2024 &copy; All Rights Reserved</li>
                     <li className='list-unstyled'>Developed with <i className="fa-solid fa-heart" style={{ color: "red" }}></i> by Toufik Gouri</li>
                     <li className='list-unstyled'><Link className='text-decoration-none' to="https://github.com/ToufikGouri">GitHub</Link></li>
-                </ul>
+                </ul> 
             </footer>
         </>
     )
